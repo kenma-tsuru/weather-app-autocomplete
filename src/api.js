76013352a7888e5622bf8d00a1e9c02ac5f0efcd
kenma-tsuru <k.pattara@outlook.com>
2022-07-15 23:56:19 +0000

    finish code along, next personalize and fine tune

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '27e9c6d715msh47cf557f5d6aca9p1f229ejsn61934b10a242',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/"
+export const WEATHER_API_KEY = "0efbef5afdbe097a759f18e5e7715ceb";
